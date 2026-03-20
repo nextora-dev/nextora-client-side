0c32c82e4bbcfa9b9cb6818cadb0f569a326c0ea
Haritha0705 <harithawikramasinha2003@gmail.com>
2026-03-20 14:25:26 +0530

    Task-2012.2: Add Meeting module dialogs and integrate meeting reducer

---
+++ components/meeting/index.ts
@@ -0,0 +1,8 @@
+export { default as MeetingDetailDialog } from './MeetingDetailDialog';
+export { default as CreateMeetingDialog } from './CreateMeetingDialog';
+export { default as AcceptMeetingDialog } from './AcceptMeetingDialog';
+export { default as RejectMeetingDialog } from './RejectMeetingDialog';
+export { default as RescheduleMeetingDialog } from './RescheduleMeetingDialog';
+export { default as FeedbackDialog } from './FeedbackDialog';
+export { default as NotesDialog } from './NotesDialog';
+export { default as CancelMeetingDialog } from './CancelMeetingDialog';
